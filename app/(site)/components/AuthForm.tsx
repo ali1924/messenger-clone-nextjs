43fa362c8020ae1382d44f0f76ca4a59706b1c43
ali1924 <mohammadali229987@gmail.com>
2023-08-17 23:05:34 +0600

    Google and Git Icon added

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -5,8 +5,10 @@ import {
     FieldValues,
     SubmitHandler
 } from 'react-hook-form';
+import { BsGithub, BsGoogle  } from 'react-icons/bs';
 import Input from '@/app/components/inputs/Input';
 import Button from '@/app/components/Button';
+import AuthSocialButton from './AuthSocialButton';
 type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
@@ -100,10 +102,59 @@ const AuthForm = () => {
                             fullWidth
                             type="submit"
                         >
-                          {variant === 'LOGIN' ? 'Sign in' : 'Register'}
+                            {variant === 'LOGIN' ? 'Sign in' : 'Register'}
                         </Button>
                     </div>
                 </form>
+
+                {/*  Divider  */}
+                <div className="mt-6">
+                    <div className="relative">
+                        <div className="
+                            absolute 
+                            inset-0 
+                            flex 
+                            items-center
+                            "
+                        >
+                            <div className="
+                                    w-full 
+                                    border-t
+                                    border-gray-300"
+                            />
+                        </div>
+                        <div className="
+                            relative 
+                            flex 
+                            justify-center 
+                            text-sm
+                            "
+                        >
+                            <span className="
+                                    bg-white 
+                                    px-2 
+                                    text-gray-500
+                                    "
+                            >
+                                Or continue with
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                {/* Social Login */}
+                <div className="
+                      mt-6 flex 
+                      gap-2
+                ">
+                   <AuthSocialButton 
+                        icon={BsGithub} 
+                        onClick={() => socialAction('github')} 
+                    />
+                    <AuthSocialButton 
+                        icon={BsGoogle} 
+                        onClick={() => socialAction('google')} 
+                    />
+                 </div>
             </div>
         </div>
     );
