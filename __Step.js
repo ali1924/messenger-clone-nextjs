85e6e427dd7a8a1c91426fde084c7ebfbfa7e0c3
ali1924 <mohammadali229987@gmail.com>
2023-08-18 01:37:16 +0600

    Mongodb connected using vs

--- __Step.js
+++ __Step.js
@@ -1,6 +1,11 @@
 /* npm i @tailwindcss/form
 change tailwind config plugin 
 
+***********Database
+          npx i -D prisma
+          npx prisma init
+          npx prisma db push
+
 ***********
 
 
