968dc5fd6664336fc8e7fe3ef44a514297194332
ali1924 <mohammadali229987@gmail.com>
2023-08-18 09:26:30 +0600

    Auth route and prismadb.ts done

--- __Step.js
+++ __Step.js
@@ -1,12 +1,36 @@
 /* npm i @tailwindcss/form
 change tailwind config plugin 
 
-***********Database
-          npx i -D prisma
-          npx prisma init
-          npx prisma db push
+***********Prisma with mongoDB connection*********
+1. npx i -D prisma
+2. npx prisma init
+3. mongodb and prisma provider setup 
+4. mongodb uri in .env (mongodb database vs uri)
+5. model create (in schema.prisma file) 
+6. npx prisma db push
 
-***********
+***********(1:18:51)Time**************
+npm i next-auth@latest  
+npm i @prisma/client 
+npm i @next-auth/prisma-adapter 
+npm i bcrypt
+npm i -D @types/bcrypt
+
+************ app>libs>prismadb.ts
+  (a)  1. import prisma client
+       2. client create (globally)
+       3. declare client type (globally)
+       4. export
+
+   (b)*********route****** app>api>auth>[...nextAuth]>route.js
+      i. const authOptions
+         i.1 adapter
+         i.2 provider
+            i.2.1 CredentialsProvider
+         i.3 debug
+      ii. NextAuth(authOptions)
+
+
 
 
 */
